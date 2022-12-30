b32116f37082962fd59dae2fd766358748eba8c7
Bryan Guffey <bryan@bryanguffey.com>
2022-12-30 12:57:07 -0800

    Initial commit

---
+++ src/globalVars.ts
@@ -0,0 +1,32 @@
+import type { Socials as SocialsType } from "./varTypes"
+import type { Person } from "./varTypes"
+
+export const PersonInfo: Person = {
+    Name: {
+        FirstName: "your first name",
+        LastName: "your last name"
+    },
+    Pronouns: "your prouns" 
+}
+
+export const Socials: SocialsType = {
+    twitterLink: "fill me in",
+    instagramLink: "fill me in", 
+    tiktokLink: "fill me in",
+    spotifyLink: "fill me in",
+    mastadonLink: "fill me in",
+    cohostLink: "fill me in"
+}
+
+export const MetaData = {
+    BaseUrl: "example-link.net",
+    ImageName: "file_name",
+    Colours: {
+        MainColour: "rgb(132, 8, 132)",
+        BackgroundColour: "#fafafa"
+    }
+
+}
+
+
+
